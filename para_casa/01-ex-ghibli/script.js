79137542d911afac7b981aa8616d58043d0f24f1
Andreza Pipolo <andreza_menezes@hotmail.com>
2022-05-06 19:15:47 -0300

    “atualização”

--- para_casa/01-ex-ghibli/script.js
+++ para_casa/01-ex-ghibli/script.js
@@ -3,7 +3,7 @@
 const filmes = [
   {
     "title": "Castle in the Sky",
-    "description": "The orphan Sheeta inherited a mysterious crystal that links her to the mythical sky-kingdom of Laputa. With the help of resourceful Pazu and a rollicking band of sky pirates, she makes her way to the ruins of the once-great civilization. Sheeta and Pazu must outwit the evil Muska, who plans to use Laputa's science to make himself ruler of the world."
+    "description": "The orphan Sheeta inherited a mysterious crystal that links her to the mythical sky-kingdom of Laputa.With the help of resourceful Pazu and a rollicking band of sky pirates, she makes her way to the ruins of the once-great civilization. Sheeta and Pazu must outwit the evil Muska, who plans to use Laputa's science to make himself ruler of the world."
   },
   {
     "title": "Grave of the Fireflies",
